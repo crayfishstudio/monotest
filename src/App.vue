88e60e3c70eeb065f4c54a55819f06da8ec3da98
Liubomyr <lybomyrdakh@gmail.com>
2022-01-31 15:10:07 +0200

    scss setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="app">
+    <header>
+      mono test task
+    </header>
+    <main>
+      <aside class="sidebar">
+        Сайдбар
+      </aside>
+      <div class="content">
+        Контент
+      </div>
+    </main>
+    <footer>
+      Футер
+    </footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
   },
 };
 </script>
 
-<style>
-#app {
+<style lang="scss">
+.app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  main {
+    display: flex;
+  }
 }
+
+
 </style>
