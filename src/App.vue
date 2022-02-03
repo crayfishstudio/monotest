006a2e051808cbfb0898a4f680bc8d6e51730c64
Liubomyr <lybomyrdakh@gmail.com>
2022-02-03 08:02:24 +0200

    basic structure

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,11 @@
     </header>
     <main>
       <aside class="sidebar">
-        Сайдбар
+        <router-link :to="'/'">Users</router-link>
+        <router-link :to="'selected'">Selected users</router-link>
       </aside>
       <div class="content">
-        Контент
+        <User/>
       </div>
     </main>
     <footer>
@@ -18,24 +19,54 @@
 </template>
 
 <script>
+import User from "./components/User.vue"
 
 export default {
   name: "App",
   components: {
+    User
   },
 };
 </script>
 
 <style lang="scss">
-.app {
+* {
+  margin: 0;
+  padding: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+.app {
+  header {
+    width: 100%;
+    background-color: #666;
+    color: #fff;
+    padding: 1rem 2rem;
+    font-size: 1.5rem;
+  }
 
   main {
     display: flex;
+
+    .sidebar {
+      width: 256px;
+      padding: 1rem 2rem;
+      border-right: 1px solid #666;
+
+      a {
+        display: block;
+
+        &:not(:last-of-type) {
+          margin-bottom: .75rem;
+        }
+      }
+    }
+
+    .content {
+
+    }
   }
 }
-
-
 </style>
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <div class="">
-
+    test
   </div>
 </template>
 
